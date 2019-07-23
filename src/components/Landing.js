567619f65b8d2412307ef1ec82a40d45152ee4d5
Ronald Prato <ronald.prato@unet.edu.ve>
2019-07-23 16:49:56 -0500

    finished login page

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react' 
+
+const Landing = () => {
+
+
+    return (
+        <div className=''>
+            
+        </div>
+    )
+}
+
+export default Landing
