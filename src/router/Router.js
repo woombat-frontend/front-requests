9ec710ad4b080ec4e4b341643b85e3c4d85b1cb4
Carlos Villalobos <carliyipxd@gmail.com>
2019-07-23 16:42:32 -0500

    [+] Create component Landing

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import Login from '../Components/Login';
+import Login from '../components/Login';
+import Landing from '../components/Landing';
 
 const Router = () => 
     <Switch>
         <Route path="/" exact component={Login}/>
+        <Route path="/landing" component={Landing}/>
     </Switch>
 
 export default Router
