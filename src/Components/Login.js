db35b4f70f49d6dde569fc28fa574c126accdb77
Carlos Villalobos <carliyipxd@gmail.com>
2019-07-23 13:06:49 -0500

    [+] Starting to create de Login page.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import '../Styles/login.css';
+import Computador from '../assets/Computador.svg';
+import JustReact from '../assets/LogoJustReact.svg';
+import WoombatLogo from'../assets/LogoWoombat.svg';
 
 const Login = () =>{
     return(
-        <div>
+        <React.Fragment>
+            <div className="container-master">
+                <div className="container-one">
+                    <img src={Computador}></img>
+                </div>
+                <div className="container-two">
 
-        </div>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
