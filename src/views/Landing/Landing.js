7b6bbe0ac39d9a2febfc1f3b3a0a96571adea7ad
Ronald Prato <ronald.prato@unet.edu.ve>
2019-07-26 16:21:04 -0500

    [+] easter egg

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -8,6 +8,7 @@ import config from '../../FirebaseConfig/auth'
 import Context from '../../GlobalState/context'
 import Swal from 'sweetalert2';
 import '../../Styles/AlertStyles.css'
+import mp3 from '../../assets/pika.mp3'
 
 
 const Landing = () => {
@@ -22,6 +23,8 @@ const Landing = () => {
         showConfirmButton: false,
         timer: 3000
     });
+    const [easterEgg, setEasterEgg] = useState([])
+    const [pikachu, setPikachu] = useState(false)
 
 
     useEffect(() => {
@@ -53,9 +56,35 @@ const Landing = () => {
         })  
     }, [])
 
+    const keyListener = k => {
+        setTimeout(() => {
+            setEasterEgg([])
+        }, 1500)
+        setEasterEgg([...easterEgg, k])
+        if (easterEgg.join('-') === '81-49-87-50-69-51-82-52-84') {
+            setPikachu(true)
+            setTimeout(() => setPikachu(false), 1500)
+            
+            var playSound = new Audio(mp3);
+            playSound.play()
+        }
+            
+        
+    }
+
     return (
         show ? 
-            <div className="container-main-landing">
+            <div tabIndex="0" onKeyDown={e => keyListener(e.keyCode)} className="container-main-landing">
+                {
+                    pikachu ? 
+                        <div className="pikachu-container"> 
+                            <audio id="pikamp3">
+                                <source src="../assets/pika.mp3" type="audio/ogg" /> 
+                            </audio>
+                            <img src="https://i.pinimg.com/originals/f3/e1/b8/f3e1b8019f160f88531d8af792716b4f.png" alt="Pikachu"/> 
+                        </div>
+                    : <div/>
+                }
                 <Header />
                 <div className="container-body-landing">
                     <LeftMenu />
