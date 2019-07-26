daec421d806c61fb764cb27b982b00b2148bc30c
Carlos Villalobos <carliyipxd@gmail.com>
2019-07-26 10:57:15 -0500

    [+] Finish create the styles to the SweetAlerts.

--- src/views/SweetAlert.js
+++ src/views/SweetAlert.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Swal from 'sweetalert2';
 import { Button, Input } from 'antd';
 import '../Styles/AlertStyles.css';
+import Context from '../GlobalState/context';
 
 const Sweet = () =>{
+
+    const {state} = useContext(Context)
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+      });
 
     const ErrorAlert = () =>{
         Swal.fire({
@@ -33,12 +43,49 @@ const Sweet = () =>{
             }       
         )
     }
+      
+      const LoginGood = () =>{
+        Toast.fire({
+            type: 'success',
+            title: state.personal_info.gender === "f" ? "Bienvenida" : "Bienvenido" + ", " + state.personal_info.name
+          })
+      }
 
+      const ProyectSendGood = () =>{
+        Toast.fire({
+            type: 'success',
+            title: 'Tu proyecto "OCR Davivienda" se ha enviado correctamente'
+          })
+      }
+
+      const ProyectSendError = () =>{
+        Toast.fire({
+            type: 'warning',
+            title: 'Hay un error, revisa el formulario'
+          })
+      }
+      const LoginFailed = () =>{
+        Toast.fire({
+            type: 'error',
+            title: 'Datos incorrectos, intente de nuevo'
+          })
+      }
+      const WizardFailedInputs = () =>{
+        Toast.fire({
+            type: 'warning',
+            title: 'Existen campos obligatorios vacios, intente de nuevo'
+          })
+      }
     return(
-        <div>
+        <div className="test">
             <h3>Sweet Alerts</h3>
             <Button onClick={ErrorAlert}>TEST ERROR</Button>
             <Button onClick={WizardQuestion}>TEST WIZARD QUESTION</Button>
+            <Button onClick={LoginGood}>TEST LOGIN SUCCESSFULLY</Button>
+            <Button onClick={ProyectSendGood}>TEST PROYECT SUCCESSFULLY SEND</Button>
+            <Button onClick={ProyectSendError}>TEST PROYECT ERROR ALERT</Button>
+            <Button onClick={LoginFailed}>TEST LOGIN ERROR ALERT</Button>
+            <Button onClick={WizardFailedInputs}>TEST WIZARD ERROR EMPTY ALERT</Button>
         </div>
     )
 }
