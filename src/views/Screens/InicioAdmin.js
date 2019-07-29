b3dfe112de781f1a4e5380670f5a65c4a76a5059
Carlos Villalobos <carliyipxd@gmail.com>
2019-07-29 15:45:53 -0500

    [+] Create filter to filter the users in the grid.

--- src/views/Screens/InicioAdmin.js
+++ src/views/Screens/InicioAdmin.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import User_male from '../../assets/User-icon.svg';
 import User_female from '../../assets/User-icon-girl.svg';
-import { Input , Icon } from 'antd';
+import { Input , Icon, Empty } from 'antd';
 import firebase from 'firebase'
 
 const InicioAdmin = () =>{
@@ -23,26 +23,53 @@ const InicioAdmin = () =>{
         })
     }, [])
 
+    const [UsersSearch, setUsersSearch] = useState("");
+    const [UsersFinals, setUsersFinals] = useState([])
+
+
+
+    const search = value => {
+        setUsersSearch(value)
+        let lowerValue = value.toLowerCase()
+        setUsersFinals(localUsers.filter(user => user.name.toLowerCase().includes(lowerValue)))
+    }
+
+
+    console.log(UsersFinals)
+
 
     return(
         <div className="container-master-inicio-admin">
             <div className="container-row-admin-proyects">
                 <div className="input-search-admin">
-                    <Icon type="search" className="icon-admin-search" /><Input placeholder="Filtrar por jefe de proyecto..." />
+                    <Icon type="search" className="icon-admin-search" /><Input placeholder="Filtrar por jefe de proyecto..." onChange={e => search(e.target.value) } />
                 </div>
             </div>
             <span className="border-span-admin"></span>
             <div className="container-row-admin-users">
                 <div className="container-master-users-admin">
-                    {localUsers.map(user =>{
-                        return(
-                            <div className="container-user-admin">
-                                {user.gender === "m" ? <img src={User_male} className="icon-user-admin"/> : <img src={User_female} className="icon-user-admin"/>}
-                                <p className="text-name-user-admin">{user.name}</p>
-                                {/* {user.notifications === null ? <div/> :<span className="span-notifications-user-admin">{user.notifications}</span>} */}
-                            </div>
-                        )
-                    })}
+                {
+                !UsersSearch.length ? 
+                localUsers.map(user =>{
+                    return(
+                        <div className="container-user-admin">
+                            {user.gender === "m" ? <img src={User_male} className="icon-user-admin"/> : <img src={User_female} className="icon-user-admin"/>}
+                            <p className="text-name-user-admin">{user.name}</p>
+                            {/* {user.notifications === null ? <div/> :<span className="span-notifications-user-admin">{user.notifications}</span>} */}
+                        </div>
+                    )
+                })
+                :
+                UsersFinals.map(user =>{
+                    return(
+                        <div className="container-user-admin">
+                            {user.gender === "m" ? <img src={User_male} className="icon-user-admin"/> : <img src={User_female} className="icon-user-admin"/>}
+                            <p className="text-name-user-admin">{user.name}</p>
+                            {/* {user.notifications === null ? <div/> :<span className="span-notifications-user-admin">{user.notifications}</span>} */}
+                        </div>
+                    )
+                })
+                }
                 </div>
             </div>
         </div>
