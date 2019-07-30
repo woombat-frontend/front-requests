2b41dbd9b9574ab3c17bc9aa62e511bcd0069d64
Carlos Villalobos <carliyipxd@gmail.com>
2019-07-30 17:02:39 -0500

    [+] Creating the view solicitudes and done the entregables view, some fixes.

---
+++ src/views/Admin/components/SubScreens/Actualizaciones.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Actualizaciones = () =>{
+    return(
+        <p>Actualizaciones</p>
+    )
+}
+
+export default Actualizaciones;
---
+++ src/views/Admin/components/SubScreens/Archivos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Archivos = () =>{
+    return(
+        <p>Archivos</p>
+    )
+}
+
+export default Archivos;
